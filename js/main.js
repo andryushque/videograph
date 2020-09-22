15bc4268a640b2cdcee4a22dd8291f6b14f65f92
andryushque <andrew.ustinov23@gmail.com>
2020-09-22 14:48:45 +0300

    Create: header media + mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  // js code
+  /*=== Mobile Menu ===*/
+  // Show and hide navigation menu
+  var mmenuButton = $(".mmenu");
+  var mmenuButtonIcon = $(".mmenu__icon");
+  var navigationMenu = $(".nav");
+  mmenuButton.on("click", function () {
+    mmenuButtonIcon.toggleClass("fa-times"); //hamburger button
+    mmenuButtonIcon.toggleClass("fa-bars"); //close button
+    navigationMenu.toggleClass("nav--hidden");
+  });
 });
