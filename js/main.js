3f765f9b1aa4e4d4c683108f116b9e1251a4a144
andryushque <andrew.ustinov23@gmail.com>
2020-09-22 15:34:05 +0300

    Create: intro section + owl-carousel

--- js/main.js
+++ js/main.js
@@ -9,4 +9,28 @@ $(document).ready(function () {
     mmenuButtonIcon.toggleClass("fa-bars"); //close button
     navigationMenu.toggleClass("nav--hidden");
   });
+
+  /*=== Owl Carousel ===*/
+  // Slider for intro section
+  $(".intro .owl-carousel").owlCarousel({
+    loop: true,
+    dots: true,
+    mouseDrag: false,
+    items: 1,
+    margin: 0,
+    smartSpeed: 1200,
+    autoHeight: false,
+    autoplay: true,
+  });
+
+  // Number of slider for every dot
+  var sliderDot = $(".intro .owl-dot");
+  sliderDot.each(function () {
+    var index = $(this).index() + 1;
+    if (index < 10) {
+      $(this).html("0").append(index);
+    } else {
+      $(this).html(index);
+    }
+  });
 });
