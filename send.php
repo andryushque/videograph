dc2f851e26d8f2761dc0908754c7196759fc829a
andryushque <andrew.ustinov23@gmail.com>
2020-09-24 13:16:04 +0300

    Fix: send,php

--- send.php
+++ send.php
@@ -28,14 +28,14 @@ try {
 
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'aundrew42@gmail.com'; // Логин на почте
-    $mail->Password   = 'nP7xQhZ-5%!hqF'; // Пароль на почте
+    $mail->Username   = 'from@gmail.com'; // Логин на почте
+    $mail->Password   = 'password'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('aundrew42@gmail.com', 'Andrew U'); // Адрес самой почты и имя отправителя
+    $mail->setFrom('from@gmail.com', 'Jack Shepard'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('andrew.ustinov23@gmail.com');  
+    $mail->addAddress('to@gmail.com');  
 
     // Отправка сообщения
     $mail->isHTML(true);
